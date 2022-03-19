c45543b1ac2c94a94cce09e9836bc287ac1c4e8d
Arman <arman.golmakan@gmail.com>
2022-03-19 00:42:45 -0400

    code cleanup. added option to sort books by title and publish date. utilized useEffect()

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main.style.css';
 import Card from './Card.js';
 
 const Main = () => {
 
     const [data, setData] = useState({ items: [] });
+    const [bookCards, setBookCards] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
+    const [order, setOrder] = useState('');
+
+    useEffect(() => {
+        let content = [];
+        if (data.docs) {
+            let sortedBooks = data.docs;
+            if (order == 'alphabetical') {
+                sortedBooks.sort((a, b) => (a.title > b.title) ? 1 : -1);
+            }
+            else if (order == 'publishdate') {
+                console.log("XXXXXXXXXXXX");
+                sortedBooks.sort((a, b) => (a.first_publish_year > b.first_publish_year) ? 1 : -1);
+            }
+            sortedBooks.map((item, index) => {
+                content.push(
+                    <li key='index'>
+                        <Card bookData={item}></Card>
+                    </li>
+                );
+            });
+            setBookCards(content);
+        }
+    }, [data, order]);
 
     const handleSubmit = async (event) => {
         setLoading(true);
@@ -16,37 +40,33 @@ const Main = () => {
         const myDataJson = await myData.json();
         setData(myDataJson);
         setLoading(false);
-        console.log(data);
-
     };
 
     const BooksSection = () => {
         return (
             <div>
                 <ContentHead />
-                <ContentBody />
+                {bookCards}
             </div>
         )
+    }
+
+    const sortByTitle = () => {
+        setOrder('alphabetical');
+    }
+
+    const sortByPublishDate = () => {
+        setOrder('publishdate');
     }
 
     const ContentHead = () => {
         return (
             <div className='contentHead'>
-                <span>{data.num_found} hits</span>
+                <span>{data.num_found} results </span>
+                <span>Sort by: <button onClick={sortByTitle}>alphabetical</button> |
+                    <button onClick={sortByPublishDate}>publish date</button></span>
             </div>
         )
-    }
-
-    const ContentBody = () => {
-        let content = [];
-        data.docs.map((item, index) => {
-            content.push(
-                <li key='index'>
-                    <Card bookData={item}></Card>
-                </li>
-            );
-        })
-        return content;
     }
 
     return (
